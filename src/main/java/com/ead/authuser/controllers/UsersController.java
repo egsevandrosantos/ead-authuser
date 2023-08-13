a00dc9a0112f3799b19f0922387eae32c0136420
Evandro Santos <egs.evandro.santos@gmail.com>
2023-08-12 21:04:03 -0300

    Config log4j2

--- src/main/java/com/ead/authuser/controllers/UsersController.java
+++ src/main/java/com/ead/authuser/controllers/UsersController.java
@@ -4,8 +4,9 @@ import com.ead.authuser.dtos.UserDTO;
 import com.ead.authuser.services.interfaces.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,28 +21,28 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
+@Log4j2
 public class UsersController {
-    Logger logger = LoggerFactory.getLogger(UsersController.class);
+//    Logger logger = LogManager.getLogger(UsersController.class);
 
     @Autowired
     private UserService service;
 
-    @GetMapping("/logging")
-    public String logging() {
-        logger.trace("TRACE");
-        logger.debug("DEBUG");
-        logger.info("INFO");
-        logger.warn("WARN");
-        logger.error("ERROR");
-        return "Logging in Spring Boot...";
-    }
+//    @GetMapping("/logging")
+//    public String logging() {
+//        log.trace("TRACE");
+//        log.debug("DEBUG");
+//        log.info("INFO");
+//        log.warn("WARN");
+//        log.error("ERROR");
+//        return "Logging in Spring Boot...";
+//    }
 
     @GetMapping
     public ResponseEntity<Page<UserDTO>> findAll(
@@ -79,13 +80,16 @@ public class UsersController {
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody @Validated(UserDTO.Create.class) @JsonView(UserDTO.Create.class) UserDTO userDTO, UriComponentsBuilder uriComponentsBuilder) {
+        log.debug("POST UsersController#create received userDTO: {}", userDTO.toString());
         if (service.valid(userDTO)) {
             UUID id = service.create(userDTO);
+            log.info("POST UsersController#create saved user id: {}", id);
             UriComponents uriComponents = uriComponentsBuilder.path("/users/{id}").buildAndExpand(id);
             return ResponseEntity
                 .created(uriComponents.toUri())
                 .build();
         } else {
+            log.warn("POST UsersController#create not valid: {}", userDTO.getErrors());
             return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(userDTO.getErrors());
@@ -94,6 +98,7 @@ public class UsersController {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable(value = "id") UUID id, @RequestBody @Validated(UserDTO.Update.class) @JsonView(UserDTO.Update.class) UserDTO userDTO) {
+        log.debug("PUT UsersController#update received id: {} and userDTO: {}", id, userDTO.toString());
         Optional<UserDTO> updatedUserDTOOpt = service.findById(id);
         if (updatedUserDTOOpt.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -103,10 +108,12 @@ public class UsersController {
         service.merge(userDTO, updatedUserDTO, UserDTO.Update.class);
         if (service.valid(updatedUserDTO)) {
             service.update(updatedUserDTO);
+            log.info("PUT UsersController#update saved user id: {}", id);
             return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
         } else {
+            log.warn("PUT UsersController#update not valid: {}", updatedUserDTO.getErrors());
             return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(updatedUserDTO.getErrors());
@@ -115,6 +122,7 @@ public class UsersController {
 
     @PutMapping("/{id}/password")
     public ResponseEntity<?> updatePassword(@PathVariable(value = "id") UUID id, @RequestBody @Validated(UserDTO.UpdatePassword.class) @JsonView(UserDTO.UpdatePassword.class) UserDTO userDTO) {
+        log.debug("PUT UsersController#updatePassword received id: {} and userDTO: {}", id, userDTO.toString());
         Optional<UserDTO> internalUserDTOOpt = service.findById(id);
         if (internalUserDTOOpt.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -126,10 +134,12 @@ public class UsersController {
         service.merge(userDTO, updatedUserDTO, UserDTO.UpdatePassword.class);
         if (service.valid(updatedUserDTO, internalUserDTO)) {
             service.update(updatedUserDTO);
+            log.info("PUT UsersController#updatePassword saved user id: {}", id);
             return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
         } else {
+            log.warn("PUT UsersController#updatePassword not valid: {}", updatedUserDTO.getErrors());
             return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(updatedUserDTO.getErrors());
@@ -138,6 +148,7 @@ public class UsersController {
 
     @PutMapping("/{id}/image")
     public ResponseEntity<?> updateImage(@PathVariable(value = "id") UUID id, @RequestBody @Validated(UserDTO.UpdateImage.class) @JsonView(UserDTO.UpdateImage.class) UserDTO userDTO) {
+        log.debug("PUT UsersController#updateImage received id: {} and userDTO: {}", id, userDTO.toString());
         Optional<UserDTO> updatedUserDTOOpt = service.findById(id);
         if (updatedUserDTOOpt.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -147,10 +158,12 @@ public class UsersController {
         service.merge(userDTO, updatedUserDTO, UserDTO.UpdateImage.class);
         if (service.valid(updatedUserDTO)) {
             service.update(updatedUserDTO);
+            log.info("PUT UsersController#updateImage saved user id: {}", id);
             return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
         } else {
+            log.warn("PUT UsersController#updateImage not valid: {}", updatedUserDTO.getErrors());
             return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(updatedUserDTO.getErrors());
@@ -159,13 +172,16 @@ public class UsersController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable(value = "id") UUID id) {
+        log.debug("DELETE UsersController#delete received id: {}", id);
         if (service.findById(id).isEmpty()) {
+            log.warn("DELETE UsersController#delete not found");
             return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .build();
         }
 
         service.deleteById(id);
+        log.info("DELETE UsersController#delete deleted user id: {}", id);
         return ResponseEntity
             .status(HttpStatus.NO_CONTENT)
             .build();
